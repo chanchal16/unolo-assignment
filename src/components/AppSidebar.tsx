f0d0b62e02193b6deef034f1249521ac19485a4d
chanchal16 <rajputchanchal17@gmail.com>
2025-02-21 20:29:12 +0530

    feat: add sidebar component

---
+++ src/components/AppSidebar.tsx
@@ -0,0 +1,122 @@
+import React from "react";
+import {
+  SidebarContent,
+  Sidebar,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  useSidebar,
+  SidebarHeader,
+} from "./ui/sidebar";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "./ui/tooltip";
+import {
+  Calendar,
+  LayoutDashboard,
+  CalendarDays,
+  ClipboardCheck,
+  Menu,
+  Building2,
+  Activity,
+  X,
+  Target,
+  ClipboardList,
+  PackageCheck,
+  CircleDollarSign,
+  Users,
+  NotebookText,
+} from "lucide-react";
+import Image from "next/image";
+
+// Menu items
+const items = [
+  { title: "Dashboard", url: "#", icon: LayoutDashboard, tooltip: "Dashboard" },
+  { title: "Attendance", url: "#", icon: CalendarDays, tooltip: "Attendance" },
+  { title: "Leaves", url: "#", icon: Calendar, tooltip: "Leaves" },
+  { title: "Organisation", url: "#", icon: Building2, tooltip: "Organisation" },
+  { title: "Tasks", url: "#", icon: ClipboardCheck, tooltip: "Tasks" },
+  { title: "Beat", url: "#", icon: Activity, tooltip: "Beat" },
+  { title: "Targets", url: "#", icon: Target, tooltip: "Targets" },
+  { title: "Form", url: "#", icon: ClipboardList, tooltip: "Form" },
+  { title: "Order", url: "#", icon: PackageCheck, tooltip: "Order" },
+  { title: "Expenses", url: "#", icon: CircleDollarSign, tooltip: "Expenses" },
+  {
+    title: "Clients & Sites",
+    url: "#",
+    icon: Users,
+    tooltip: "Clients & Sites",
+  },
+  { title: "Report", url: "#", icon: NotebookText, tooltip: "Report" },
+];
+
+const AppSidebar = () => {
+  const { openMobile, setOpenMobile, isMobile } = useSidebar();
+
+  const toggleSidebar = () => {
+    setOpenMobile(!openMobile);
+  };
+
+  return (
+    <TooltipProvider>
+      {isMobile && (
+        <button
+          className="p-2 m-2 bg-blue-500 text-white rounded fixed top-0 left-0 z-50"
+          onClick={toggleSidebar}
+        >
+          {openMobile ? <X /> : <Menu />}
+        </button>
+      )}
+      <Sidebar
+        className={`!bg-white ${isMobile && !openMobile ? "hidden" : ""}`}
+      >
+        <SidebarHeader>
+          <Image
+            className="m-auto"
+            src={"/logo.jpeg"}
+            alt="logo"
+            width={26}
+            height={26}
+          />
+        </SidebarHeader>
+        <SidebarContent className="p-2">
+          <SidebarMenu>
+            {items.map((item) => (
+              <SidebarMenuItem key={item.title}>
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                    <SidebarMenuButton tooltip={item.tooltip} asChild>
+                      <a
+                        href={item.url}
+                        className={`flex flex-col gap-2 h-auto ${
+                          item.title === "Dashboard"
+                            ? "bg-indigo-50"
+                            : "bg-transparent"
+                        }`}
+                      >
+                        <item.icon
+                          className={`!h-6 !w-6 ${
+                            item.title === "Dashboard"
+                              ? "text-indigo-500"
+                              : "text-gray-400"
+                          }  `}
+                        />
+                        {/* <span className="text-xs">{item.title}</span> */}
+                      </a>
+                    </SidebarMenuButton>
+                  </TooltipTrigger>
+                  <TooltipContent side="right">{item.tooltip}</TooltipContent>
+                </Tooltip>
+              </SidebarMenuItem>
+            ))}
+          </SidebarMenu>
+        </SidebarContent>
+      </Sidebar>
+    </TooltipProvider>
+  );
+};
+
+export default AppSidebar;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
+import AppSidebar from "@/components/AppSidebar";
+import { SidebarProvider } from "@/components/ui/sidebar";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <SidebarProvider>
+      <div className="flex min-h-screen">
+        <AppSidebar />
+        <Component {...pageProps} />
+      </div>
+    </SidebarProvider>
+  );
 }
