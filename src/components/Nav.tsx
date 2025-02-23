37ee04150f2377a6b75448346dc904c73f5e74d6
chanchal16 <rajputchanchal17@gmail.com>
2025-02-23 22:56:13 +0530

    update: change initals for logged in user

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { Bell, MessageSquareText } from "lucide-react";
+import { useUser } from "@clerk/nextjs";
+import { getInitials } from "@/lib/getInitials";
 
 const Nav = () => {
+  const { user } = useUser();
+  const initials = getInitials(user?.firstName ?? "U", user?.lastName ?? "");
   return (
     <div className="p-2 w-full">
       <ul className="flex gap-4 px-2 items-center justify-end text-gray-500">
@@ -13,7 +17,7 @@ const Nav = () => {
         </li>
         <li>
           <div className="bg-indigo-50 text-indigo-500 p-2.5 cursor-pointer text-sm rounded-full">
-            SK
+            {initials}
           </div>
         </li>
       </ul>
